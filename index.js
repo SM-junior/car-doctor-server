d9e0fc6563f6c802997e8083f9414da19a3798d9
SM-junior <shahinalam12341@gmail.com>
2023-10-29 01:50:13 +0600

    trying

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const serviceCollection=client.db('carDoctor').collection('services');
+    const bookingCollection=client.db('carDoctor').collection('bookings');
 
     app.get('/services', async(req,res)=>{
         const cursor=serviceCollection.find();
@@ -38,13 +39,19 @@ async function run() {
         const query={_id: new ObjectId(id)}
 
         const options = {
-            
             // _id automatic asbe, title & price ta cai tai, bakigula jemon description img-url agula ke cai na, eta khub important
-            projection: {title: 1, price: 1 },
+            projection: {title: 1, price: 1,},
           };
 
         const result=await serviceCollection.findOne(query,options)
         res.send(result)
+    })
+
+    app.post('/bookings', async(req,res)=>{
+      const booking=req.body;
+      const result=await bookingCollection.insertOne(booking)
+      res.send(result)
+      console.log(booking);
     })
 
     await client.db("admin").command({ ping: 1 });
