fb57b37b2277c0047ace7c6f518b506d50b86a55
SM-junior <shahinalam12341@gmail.com>
2023-11-03 12:14:22 +0600

    almost done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
-const express=require('express')
+const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors=require('cors')
-const app=express()
-const port=process.env.PORT || 3000;
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000;
 
 app.use(cors())
-app.use(express.json()) 
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n2wd0zs.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,33 +25,53 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
 
-    const serviceCollection=client.db('carDoctor').collection('services');
-    const bookingCollection=client.db('carDoctor').collection('bookings');
+    const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
 
-    app.get('/services', async(req,res)=>{
-        const cursor=serviceCollection.find();
-        const result=await cursor.toArray()
-        res.send(result)
-    })
-
-    app.get('/services/:id', async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)}
-
-        const options = {
-            // _id automatic asbe, title & price ta cai tai, bakigula jemon description img-url agula ke cai na, eta khub important
-            projection: {title: 1, price: 1,},
-          };
-
-        const result=await serviceCollection.findOne(query,options)
-        res.send(result)
-    })
-
-    app.post('/bookings', async(req,res)=>{
-      const booking=req.body;
-      const result=await bookingCollection.insertOne(booking)
+    //getting all service data from database
+    app.get('/services', async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray()
       res.send(result)
-      console.log(booking);
+    })
+
+    //getting a single service data from all service data from database
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const options = {
+        // _id automatic asbe, title & price ta cai tai, bakigula jemon description img-url agula ke cai na, eta khub important
+        projection: { title: 1, price: 1, img: 1 }
+      };
+
+      const result = await serviceCollection.findOne(query, options)
+      res.send(result)
+    })
+
+    //inserting single booking data from user interface to database
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking)
+      res.send(result)
+    })
+
+    //getting some booking data from database by booking with same email
+    app.get('/bookings', async (req, res) => {
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const cursor = bookingCollection.find(query)
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
@@ -63,11 +83,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/',(req,res)=>{
-    res.send('doctor is running')
+app.get('/', (req, res) => {
+  res.send('doctor is running')
 })
 
-app.listen(port,()=>{
-    console.log(`doctor is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`doctor is running on port: ${port}`);
 })
